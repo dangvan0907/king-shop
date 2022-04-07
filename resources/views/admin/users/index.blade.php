6257527756de70b259a6603b1c134b01f8296296
Dang Van <danghuuvan090700@gmail.com>
2022-04-07 23:25:38 +0700

    edit view user index and show

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,52 @@
-<?php
+@extends('layouts.teamplate')
+@section('content')
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">DataTable with minimal features & hover style</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table id="example2" class="table table-bordered table-hover">
+                                <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th></th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($users as $user)
+                                    <tr>
+                                        <td>{{$user->id}}</td>
+                                        <td>{{$user->name}}</td>
+                                        <td>{{$user->email}}</td>
+                                        <td></td>
+                                    </tr>
+                                @endforeach
+
+                                </tbody>
+                                <tfoot>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th></th>
+                                </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+@endsection
--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -1,1 +1,16 @@
-<?php
+@extends('layouts.teamplate')
+@section('content')
+    <section class="content">
+        <div class="card card-primary card-outline">
+            <div class="card-body box-profile">
+
+                <h3 class="profile-username text-center">{{$user->name}}</h3>
+
+                <p class="text-muted text-center">{{$user->email}}</p>
+                <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>
+            </div>
+            <!-- /.card-body -->
+        </div>
+
+    </section>
+@endsection
