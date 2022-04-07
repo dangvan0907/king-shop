aeb8840888db4791dacee49af064c3c54db0623c
Dang Van <danghuuvan090700@gmail.com>
2022-04-07 21:40:48 +0700

    create views admin

---
+++ resources/views/layouts/teamplate.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Dashboard 2</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="{{asset("assert/plugins/fontawesome-free/css/all.min.css")}}">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="{{asset("assert/plugins/overlayScrollbars/css/OverlayScrollbars.min.css")}}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset("assert/dist/css/adminlte.min.css")}}">
+</head>
+<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+<div class="wrapper">
+
+
+    @include('layouts.partials.preloader')
+    @include('layouts.partials.navbar')
+    @include('layouts.partials.sidebar')
+    <div class="content-wrapper">
+
+        @include('layouts.partials.headerconten')
+
+
+
+        @yield('content')
+    </div>
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+    </aside>
+    @include('layouts.partials.footer')
+
+
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+<!-- jQuery -->
+<script src="{{asset("assert/plugins/jquery/jquery.min.js")}}"></script>
+<!-- Bootstrap -->
+<script src="{{asset("assert/plugins/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
+<!-- overlayScrollbars -->
+<script src="{{asset("assert/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js")}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset("assert/dist/js/adminlte.js")}}"></script>
+
+<!-- PAGE PLUGINS -->
+<!-- jQuery Mapael -->
+<script src="{{asset("assert/plugins/jquery-mousewheel/jquery.mousewheel.js")}}"></script>
+<script src="{{asset("assert/plugins/raphael/raphael.min.js")}}"></script>
+<script src="{{asset("assert/plugins/jquery-mapael/jquery.mapael.min.js")}}"></script>
+<script src="{{asset("assert/plugins/jquery-mapael/maps/usa_states.min.js")}}"></script>
+<!-- ChartJS -->
+<script src="{{asset("assert/plugins/chart.js/Chart.min.js")}}"></script>
+
+<!-- AdminLTE for demo purposes -->
+<script src="{{asset("assert/dist/js/demo.js")}}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{asset("assert/dist/js/pages/dashboard2.js")}}"></script>
+</body>
+</html>
