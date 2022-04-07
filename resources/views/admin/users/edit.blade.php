0db201f710abf69b827d21f5d5eed55066d49de6
Dang Van <danghuuvan090700@gmail.com>
2022-04-07 23:24:59 +0700

    edit view user edit

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -1,1 +1,43 @@
-<?php
+@extends('layouts.teamplate')
+@section('content')
+    <section class="content">
+        <form action="{{route('users.update',$user->id)}}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">General</h3>
+
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="inputName">Name</label>
+                                <input name="name" type="text" id="inputName" class="form-control" value="{{$user->name}}">
+                            </div>
+                            <div class="form-group">
+                                <label for="inputEmail">Email</label>
+                                <input name="email" type="text" id="inputEmail" class="form-control" value="{{$user->email}}">
+                            </div>
+
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <a href="#" class="btn btn-secondary">Cancel</a>
+                    <input type="submit" value="Save Changes" class="btn btn-success float-right">
+                </div>
+            </div>
+        </form>
+    </section>
+@endsection
