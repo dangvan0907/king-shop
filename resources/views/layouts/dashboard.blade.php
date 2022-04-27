767d8a436018daefe2f9b0d6ab6bc30c2fafda0e
Dang Van <danghuuvan090700@gmail.com>
2022-04-27 08:48:45 +0700

    hot fix

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -13,7 +13,10 @@
                         <div class="icon">
                             <i class="ion ion-bag"></i>
                         </div>
+
+                        @hasPermission('index-product')
                         <a href="{{route('products.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        @endhasPermission
                     </div>
                 </div>
                 <div class="col-lg-3 col-6">
@@ -25,7 +28,9 @@
                         <div class="icon">
                             <i class="ion ion-stats-bars"></i>
                         </div>
+                        @hasPermission('index-category')
                         <a href="{{route('categories.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        @endhasPermission
                     </div>
                 </div>
                 <div class="col-lg-3 col-6">
@@ -37,7 +42,9 @@
                         <div class="icon">
                             <i class="ion ion-person-add"></i>
                         </div>
+                        @hasPermission('index-user')
                         <a href="{{route('users.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        @endhasPermission
                     </div>
                 </div>
                 <!-- ./col -->
@@ -51,7 +58,9 @@
                         <div class="icon">
                             <i class="ion ion-pie-graph"></i>
                         </div>
+                        @hasPermission('index-role')
                         <a href="{{route('roles.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        @endhasPermission
                     </div>
                 </div>
                 <!-- ./col -->
