3a4f12e24bcc7514cd6d53dcd7152cb996422cf6
Dang Van <danghuuvan090700@gmail.com>
2022-04-29 10:57:15 +0700

    edit test role getshow

--- tests/Feature/Role/GetShowRoleTest.php
+++ tests/Feature/Role/GetShowRoleTest.php
@@ -11,7 +11,7 @@ use Tests\TestCase;
 class GetShowRoleTest extends TestCase
 {
     /** @test */
-    public function unauthenticatedUserCanNotGetSingleRole()
+    public function unauthenticated_user_can_not_get_single_role()
     {
         $role = Role::factory()->create();
         $response = $this->get($this->getShowRoleRoute($role->id));
@@ -21,11 +21,11 @@ class GetShowRoleTest extends TestCase
     }
 
     /** @test */
-    public function authenticatedSuperAdminCanGetSingleRole()
+    public function authenticated_super_admin_can_get_single_role()
     {
         $this->loginWithSuperAdmin();
         $role = Role::factory()->create();
-        $response = $this->get($this->getShowRoleRoute($role->id));
+        $response = $this->get($this->getShowRoleRoute( $role->id));
 
         $response->assertStatus(Response::HTTP_OK);
         $response->assertViewIs('admin.roles.show');
@@ -33,21 +33,21 @@ class GetShowRoleTest extends TestCase
     }
 
     /** @test */
-    public function authenticatedSuperAdminCanNotGetSingleRoleIfRoleIsNotExist()
+    public function authenticated_super_admin_can_not_get_single_role_if_role_is_not_exist()
     {
         $this->loginWithSuperAdmin();
         $roleId = -1;
-        $response = $this->get($this->getShowRoleRoute($roleId));
+        $response = $this->get($this->getShowRoleRoute( $roleId));
 
         $response->assertStatus(Response::HTTP_NOT_FOUND);
     }
 
     /** @test */
-    public function authenticatedUserHavePermissionCanGetSingleRole()
+    public function authenticated_user_have_permission_can_get_single_role()
     {
         $this->loginUserWithPermission('index-role');
         $role = Role::factory()->create();
-        $response = $this->get($this->getShowRoleRoute($role->id));
+        $response = $this->get($this->getShowRoleRoute( $role->id));
 
         $response->assertStatus(Response::HTTP_OK);
         $response->assertViewIs('admin.roles.show');
@@ -55,11 +55,11 @@ class GetShowRoleTest extends TestCase
     }
 
     /** @test */
-    public function authenticatedUserHavePermissionCanNotGetSingleRoleIfRoleIsNotExist()
+    public function authenticated_user_have_permission_can_not_get_single_role_if_role_is_not_exist()
     {
         $this->loginUserWithPermission('index-role');
         $roleId = -1;
-        $response = $this->get($this->getShowRoleRoute($roleId));
+        $response = $this->get($this->getShowRoleRoute( $roleId));
 
         $response->assertStatus(Response::HTTP_NOT_FOUND);
     }
